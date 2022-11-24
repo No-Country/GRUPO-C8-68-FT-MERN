c9d7c552265ad7127e9b8c9546f764fb415a17f9
AriaGT <ariagt191000@gmail.com>
2022-11-24 02:47:30 -0500

    First Login System Config

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,18 @@
-import axios from 'axios';
-import { useEffect } from 'react';
-import { useState } from 'react';
 import { HashRouter, Routes, Route } from 'react-router-dom'
 import { AppContainer } from './AppGlobalStyles';
 import { DefaultFooter } from './Components/footer/default-footer';
 import { DefaultNavbar } from './Components/navbar/default-navbar';
+import Card from './pages/Card';
 import CreateNewPassword from './pages/CreateNewPassword';
 import ForgotPassword from './pages/ForgotPassword';
 import HomePage from './pages/HomePage';
+import Library from './pages/Library';
 import LoginPage from './pages/LoginPage';
+import ProtectedRoutes from './pages/ProtectedRoutes';
+import Purchases from './pages/Purchases';
 import RegisterPage from './pages/RegisterPage';
 
 function App() {
-
-  const [games, setGames] = useState()
-
-  useEffect(() => {
-    const URL = 'https://nc8-68backend-production.up.railway.app/games'
-
-    axios.get(URL)
-      .then(res => setGames(res.data))
-      .catch(err => console.log(err))
-  }, [])
-
   return (
     <HashRouter>
       <AppContainer>
@@ -33,6 +23,12 @@ function App() {
               <Route path='/forgotPassword' element={<ForgotPassword />} />
               <Route path='/createNewPassword' element={<CreateNewPassword />} />
               <Route path='/register' element={<RegisterPage />} />
+              
+              <Route element={<ProtectedRoutes />}>
+                <Route path='/cart' element={<Card />} />
+                <Route path='/library' element={<Library />} />
+                <Route path='/purchases' element={<Purchases />} />
+              </Route>
             </Routes>
         <DefaultFooter />
       </AppContainer>
