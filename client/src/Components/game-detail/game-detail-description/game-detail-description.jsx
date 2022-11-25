f14221177ba573833ba4762fe9a417e84803ca46
fabianeromano <fabian.ed.romano@gmail.com>
2022-11-25 13:28:38 -0300

    Agregando detail a las cards

---
+++ client/src/Components/game-detail/game-detail-description/game-detail-description.jsx
@@ -0,0 +1,9 @@
+import { GameDetailDescriptionContainer } from "./styles";
+
+export const GameDetailDescription = () => {
+    return ( 
+<GameDetailDescriptionContainer>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi tempore optio nihil! Incidunt ipsam sed atque alias amet corporis expedita, voluptates sint aspernatur deleniti? Architecto, quidem! Sequi veniam atque dolores deserunt, officiis deleniti placeat perferendis asperiores consectetur nesciunt minus eveniet omnis aut quis enim natus. Dignissimos corrupti consequatur delectus atque.</p>
+</GameDetailDescriptionContainer>
+     );
+}
