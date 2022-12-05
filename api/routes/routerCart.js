9614e4412d8978a939406ad84c348a3631f5e1f9
gandhyman <gandhycoder@gmail.com>
2022-12-05 15:32:23 -0700

    Add: /cart/paid route

--- api/routes/routerCart.js
+++ api/routes/routerCart.js
@@ -6,5 +6,6 @@ const cartController = require('../controllers/cartController')
 
 router.get('/', cartController.getCart)
 router.post('/', cartController.addCart)
+router.post('/paid', cartController.addOrder)
 
 module.exports = router
