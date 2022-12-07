1fa7c80bd57e33b40106c46f805e5703e64526da
gandhyman <gandhycoder@gmail.com>
2022-12-06 17:08:01 -0700

    Update: fix cart

--- client/src/store/slices/cart.slice.js
+++ client/src/store/slices/cart.slice.js
@@ -1,24 +1,27 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit'
 import getToken from '../../utils/getToken'
 import { useSelector } from 'react-redux'
-import axios from "axios";
+import axios from 'axios'
 
 const cartSlice = createSlice({
   name: 'cart',
   initialState: null,
   reducers: {
-    setCartGlobal: (state, action) => action.payload
-  }
+    setCartGlobal: (state, action) => action.payload,
+  },
 })
 export const { setCartGlobal } = cartSlice.actions
 
 export default cartSlice.reducer
 
-console.log(getToken())
-
 export const getAllProductsCart = () => (dispatch) => {
-  const URL = 'https://grupo-c8-68-ft-mern-production.up.railway.app/cart'
-  return axios.get(URL, getToken())
-    .then(res => dispatch(setCartGlobal(res.data)))
-    .catch(err => console.log(err))
-} 
+  console.log('get token slice', getToken())
+  const URL = 'https://grupo-c8-68-ft-mern-production.up.railway.app/cart/get'
+  return axios
+    .post(URL, getToken()) //
+    .then((res) => {
+      console.log('then cart slice', res.data)
+      dispatch(setCartGlobal(res.data))
+    })
+    .catch((err) => console.log('error catch en car slice', err))
+}
