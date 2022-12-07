4f73ec1f6a51b3fab6408fbd1314719f7c8bf7ad
gandhyman <gandhycoder@gmail.com>
2022-12-07 10:22:32 -0700

    update: token

--- client/src/utils/getToken.js
+++ client/src/utils/getToken.js
@@ -1,6 +1,12 @@
-const getToken = () => ({
-  token: JSON.parse(localStorage.getItem('applicationState')).user.user.token,
-})
+const getToken = () => {
+  const token = JSON.parse(localStorage.getItem('applicationState')).user.user
+    .token
+  if (token)
+    return {
+      token,
+    }
+  else return null
+}
 //   {
 //     headers: {
 //       Authorization: `JWT ${JSON.parse(localStorage.getItem('applicationState')).user.user.token}`
